593bf7fb83e60c190c2f08aad4c3ee585ab94aa7
Luoxi Pan <l.panpax@gmail.com>
2022-04-26 21:45:24 +1000

    Attach logging function to form submission

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-// function onSubmit(e) {
-//     console.log(`Form Submitted! Time stamp: ${e.timeStamp}`);
-//     e.preventDefault();
+function onSubmit(e) {
+    console.log(`Form Submitted! Time stamp: ${e.timeStamp}`);
+    e.preventDefault();
+}
 
-//     inputs = Array.from(document.getElementsByClassName("error"));
-//     inputs.forEach(input => {
-//         input.classList.add("submitted");
-//     });
-// }
-
-// form = document.getElementById("recruitment-form")
-// form.addEventListener('submit', onSubmit);
+form = document.getElementById("recruitment-form")
+form.addEventListener('submit', onSubmit);
 
 // console.log(form);
 // console.log(buttons);
