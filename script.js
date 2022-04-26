03bac5dd7b5d19c5ce30fbc58eb9372b77645f11
Luoxi Pan <l.panpax@gmail.com>
2022-04-26 21:39:18 +1000

    Get error coloring to play nice with required

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Courage duty honour!");
+// function onSubmit(e) {
+//     console.log(`Form Submitted! Time stamp: ${e.timeStamp}`);
+//     e.preventDefault();
+
+//     inputs = Array.from(document.getElementsByClassName("error"));
+//     inputs.forEach(input => {
+//         input.classList.add("submitted");
+//     });
+// }
+
+// form = document.getElementById("recruitment-form")
+// form.addEventListener('submit', onSubmit);
+
+// console.log(form);
+// console.log(buttons);
