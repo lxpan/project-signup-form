3d274c3fb24336a7be3997dbaaea9731767b7a07
Luoxi Pan <l.panpax@gmail.com>
2022-04-28 20:08:06 +1000

    Add error class for password fields

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
+function checkPasswordMatch(e) {
+    e.preventDefault();
+    let password = document.getElementById("password");
+    let confirmPassword = document.getElementById("passwordConfirm");
+
+    if(password.value !== confirmPassword.value) {
+        console.log("Password must match!");
+        password.classList.add('error');
+        passwordConfirm.classList.add('error');
+    } else {
+        console.log("Passwords OK!")
+    }
+}
+
 function onSubmit(e) {
     console.log(`Form Submitted! Time stamp: ${e.timeStamp}`);
     e.preventDefault();
 }
 
 form = document.getElementById("recruitment-form")
-form.addEventListener('submit', onSubmit);
 
+
+form.addEventListener('submit', checkPasswordMatch);
 // console.log(form);
 // console.log(buttons);
