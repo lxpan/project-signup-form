01c0596cbc8a46768002896fa9baf040e8e7e7b3
Luoxi Pan <l.panpax@gmail.com>
2022-04-28 20:25:31 +1000

    Add password matching validation to form

--- script.js
+++ script.js
@@ -2,24 +2,25 @@ function checkPasswordMatch(e) {
     e.preventDefault();
     let password = document.getElementById("password");
     let confirmPassword = document.getElementById("passwordConfirm");
+    let passwordMatch = document.querySelector("#passwordMatch > span");
 
     if(password.value !== confirmPassword.value) {
         console.log("Password must match!");
         password.classList.add('error');
         passwordConfirm.classList.add('error');
+        passwordMatch.classList.add('error');
     } else {
         console.log("Passwords OK!")
+        password.classList.remove('error');
+        passwordConfirm.classList.remove('error');
+        passwordMatch.classList.remove('error');
     }
 }
 
 function onSubmit(e) {
-    console.log(`Form Submitted! Time stamp: ${e.timeStamp}`);
     e.preventDefault();
+    console.log(`Form Submitted! Time stamp: ${e.timeStamp}`);
 }
 
-form = document.getElementById("recruitment-form")
-
-
+form = document.getElementById("recruitment-form");
 form.addEventListener('submit', checkPasswordMatch);
-// console.log(form);
-// console.log(buttons);
